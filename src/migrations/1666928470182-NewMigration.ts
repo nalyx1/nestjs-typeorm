288bede5ffdeb7814fd4a1790be1c6001287ea2e
nalyx1 <joaorodriguez0106@gmail.com>
2022-11-04 00:05:20 -0300

    table users_skill fix

--- src/migrations/1666928470182-NewMigration.ts
+++ src/migrations/1666928470182-NewMigration.ts
@@ -11,28 +11,28 @@ export class NewMigration1666928470182 implements MigrationInterface {
       `CREATE TABLE "skill" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, CONSTRAINT "PK_a0d33334424e64fb78dc3ce7196" PRIMARY KEY ("id"))`,
     );
     await queryRunner.query(
-      `CREATE TABLE "users_skills_skill" ("usersId" uuid NOT NULL, "skillId" uuid NOT NULL, CONSTRAINT "PK_576a37a8cc61d3dbecfb66eb4ce" PRIMARY KEY ("usersId", "skillId"))`,
+      `CREATE TABLE "users_skills" ("usersId" uuid NOT NULL, "skillId" uuid NOT NULL, CONSTRAINT "PK_576a37a8cc61d3dbecfb66eb4ce" PRIMARY KEY ("usersId", "skillId"))`,
     );
     await queryRunner.query(
-      `CREATE INDEX "IDX_a443411b588c6603a9205e0ede" ON "users_skills_skill" ("usersId") `,
+      `CREATE INDEX "IDX_a443411b588c6603a9205e0ede" ON "users_skills" ("usersId") `,
     );
     await queryRunner.query(
-      `CREATE INDEX "IDX_9de18e774763eb53aa1044809d" ON "users_skills_skill" ("skillId") `,
+      `CREATE INDEX "IDX_9de18e774763eb53aa1044809d" ON "users_skills" ("skillId") `,
     );
     await queryRunner.query(
-      `ALTER TABLE "users_skills_skill" ADD CONSTRAINT "FK_a443411b588c6603a9205e0edeb" FOREIGN KEY ("usersId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE`,
+      `ALTER TABLE "users_skills" ADD CONSTRAINT "FK_a443411b588c6603a9205e0edeb" FOREIGN KEY ("usersId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE`,
     );
     await queryRunner.query(
-      `ALTER TABLE "users_skills_skill" ADD CONSTRAINT "FK_9de18e774763eb53aa1044809d4" FOREIGN KEY ("skillId") REFERENCES "skill"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+      `ALTER TABLE "users_skills" ADD CONSTRAINT "FK_9de18e774763eb53aa1044809d4" FOREIGN KEY ("skillId") REFERENCES "skill"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE "users_skills_skill" DROP CONSTRAINT "FK_9de18e774763eb53aa1044809d4"`,
+      `ALTER TABLE "users_skills" DROP CONSTRAINT "FK_9de18e774763eb53aa1044809d4"`,
     );
     await queryRunner.query(
-      `ALTER TABLE "users_skills_skill" DROP CONSTRAINT "FK_a443411b588c6603a9205e0edeb"`,
+      `ALTER TABLE "users_skills" DROP CONSTRAINT "FK_a443411b588c6603a9205e0edeb"`,
     );
     await queryRunner.query(
       `DROP INDEX "public"."IDX_9de18e774763eb53aa1044809d"`,
@@ -40,7 +40,7 @@ export class NewMigration1666928470182 implements MigrationInterface {
     await queryRunner.query(
       `DROP INDEX "public"."IDX_a443411b588c6603a9205e0ede"`,
     );
-    await queryRunner.query(`DROP TABLE "users_skills_skill"`);
+    await queryRunner.query(`DROP TABLE "users_skills"`);
     await queryRunner.query(`DROP TABLE "skill"`);
     await queryRunner.query(`DROP TABLE "users"`);
   }
